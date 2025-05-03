8264d2ee1206faffc647d38a8d4374d3fd77d29f
wptasmina <wptasmina@gmail.com>
2025-05-03 19:31:33 +0600

    added navbar

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
       <header className="flex w-full items-center justify-between bg-gradient-to-tl from-white from-40% via-[#004FCE]/10 via-70% to-[#004FCE]/20 to-100% px-4 py-4 backdrop-blur-lg">
-        <div className="container mx-auto max-w-7xl">
+        <div className="w-10/12 mx-auto max-w-7xl">
           <nav className="flex items-center justify-between">
             <div className="flex items-center space-x-8">
               <div className="flex items-center space-x-1">
